a8316df2a6daeaa6eb815e7e6cb7547bab671f06
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-11-25 19:56:37 +0100

    Add functions add, substract, multiply, divide to main.js

---
+++ main.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+
+console.log(add(4, 5));
+console.log(substract(4, 5))
+console.log(multiply(5, 5));
+console.log(divide(5, 15));
