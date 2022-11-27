359ccecd0b5e0d14ea1311d1ab29fe7b727092aa
Marek-Kosmowski <kosmowski.marek@gmail.com>
2022-11-27 22:19:12 +0100

    Add for each operator buttons functions to test if works

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const plus = document.querySelector('.plus')
+const minus = document.querySelector('.minus')
+const slash = document.querySelector('.slash')
+const star = document.querySelector('.star')
+const operatorButtons = document.querySelectorAll('.operator')
+
 function add(a, b) {
     return a + b;
 }
@@ -15,7 +21,44 @@ function divide(a, b) {
 }
 
 
-console.log(add(4, 5));
-console.log(substract(4, 5))
-console.log(multiply(5, 5));
-console.log(divide(5, 15));
+// WTF AM I DOING HERE M8 !!!!
+// function operate(operator) {
+//     if (operator.target.className === 'plus') {
+//         console.log('works');
+//     } else if (operator === minus) {
+//         console.log(substract(5, 3));
+
+//     } else if (operator === slash) {
+//         console.log(divide(4, 2));
+
+//     } else if (operator === star) {
+//         console.log(multiply(3, 5));
+//     }
+// }
+
+
+// operatorButtons.forEach(btn => {
+//     btn.addEventListener('click', operate);
+// })
+
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', (operator) => {
+        if (btn.classList.contains('plus')) {
+            console.log(add(3, 2));
+        } else if (btn.classList.contains('minus')) {
+            console.log(substract(4, 2));
+        } else if (btn.classList.contains('slash')) {
+            console.log(divide(10, 2));
+        } else if (btn.classList.contains('star')) {
+            console.log(multiply(7, 5));
+        }
+    })
+})
+
+// document.body.addEventListener('click', operate);
+
+
+// console.log(add(4, 5));
+// console.log(substract(4, 5))
+// console.log(multiply(5, 5));
+// console.log(divide(5, 15));
